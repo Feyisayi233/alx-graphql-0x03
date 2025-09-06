769189e051f9cc26351a5096f0a2b480fc8fe4d9
Feyisayo <feyisayo233@gmail.com>
2025-09-06 17:00:00 +0100

    Update _app.tsx

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -1,11 +1,16 @@
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import ApolloProvider from "@/graphql/ApolloProvider";
+import ErrorBoundary from "@/components/ErrorBoundary";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider>
-      <Component {...pageProps} />
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
     </ApolloProvider>
   );
 }
