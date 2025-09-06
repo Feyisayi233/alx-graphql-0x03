668c6e838a8f55ec4836aa2c7975761a3201a6aa
Feyisayo <feyisayo233@gmail.com>
2025-09-06 17:04:39 +0100

    Add ErrorProneComponent and use ErrorBoundary in Home
    Introduced a new ErrorProneComponent that throws a test error and wrapped it with ErrorBoundary in the Home page for error handling demonstration. Updated imports accordingly. No changes to application logic outside of error handling showcase.

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -1,9 +1,7 @@
-
-import "@/styles/globals.css";
+import "../app/globals.css";
 import type { AppProps } from "next/app";
 import ApolloProvider from "@/graphql/ApolloProvider";
 import ErrorBoundary from "@/components/ErrorBoundary";
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
